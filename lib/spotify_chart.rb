0803055b2eb01f173476e5ef119eaf0784040325
jmoses89 <jarret.moses@gmail.com>
2014-10-08 10:05:15 -0400

    parsing spotify is fun

--- lib/spotify_chart.rb
+++ lib/spotify_chart.rb
@@ -1,24 +1,38 @@
 require 'json'
 require 'open-uri'
+require 'pry'
 
 class SpotifyChart
 
   attr_reader :base_url
 
   def initialize
-    @base_url = "?"
+    @base_url = "http://charts.spotify.com/api/charts/"
   end
 
   def get_url(preference, region)
     # return a string that is the base url + / + preference + / + region + / + latest
+    string = "#{@base_url}#{preference}/#{region}/latest"
+    # binding.pry
   end
 
   def get_json(url)
     # load json given a url here
     # refer to the references if you have questions about this
+    spotify_json = JSON.load(open(url))
+
   end
 
   def fetch_track_album_artist(music_hash)
+    # binding.pry
+    info = music_hash["tracks"]
+    artist = info[0]["artist_name"]
+    album = info[0]["album_name"]
+    song = "'#{info[0]["track_name"]}'"
+
+
+    result = "#{song} by #{artist} from the album #{album}"
+    # binding.pry
     # example music_hash:
     # {  
     #   "tracks" => [  
@@ -46,11 +60,13 @@ class SpotifyChart
 
 
   def most_streamed(region)
+    url = get_url('most_streamed','us')
+    # binding.pry
     # call no get_url here, where preference is the string 'most_streamed',
     # and set it equal to a variable
-
+    spotify_json = get_json(url)
     # call on get_json here, using the string that get_url returns
-    
+    fetch_track_album_artist(spotify_json)
     # finally, call on fetch_track_album_artist using the 
     # hash that get_json returns
   end
@@ -58,11 +74,12 @@ class SpotifyChart
   def most_shared(region)
     # call no get_url here, where preference is the string 'most_shared',
     # and set it equal to a variable
-
+    url = get_url('most_shared', 'us')
     # call on get_json here, using the string that get_url returns
-    
+    json = get_json(url)
     # finally, call on fetch_track_album_artist using the 
     # hash that get_json returns
+    fetch_track_album_artist(json)
   end
 
 end
